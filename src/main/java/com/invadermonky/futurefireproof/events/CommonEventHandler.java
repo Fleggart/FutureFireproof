ba0d54839b2725a29fe7d6e47bb910a480d8f16f
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-06 18:49:15 +0800

    Refactor entity item handling in CommonEventHandler

--- src/main/java/com/invadermonky/futurefireproof/events/CommonEventHandler.java
+++ src/main/java/com/invadermonky/futurefireproof/events/CommonEventHandler.java
@@ -1,8 +1,7 @@
 package com.invadermonky.futurefireproof.events;
 
+import com.invadermonky.futurefireproof.FutureFireproof;
 import com.invadermonky.futurefireproof.entity.EntityFireproofItem;
-import com.invadermonky.futurefireproof.entity.EntityFireproofItemLootHandler;
-import com.invadermonky.futurefireproof.registry.RegistrarFF;
 import com.invadermonky.futurefireproof.util.FireproofHelper;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
@@ -11,29 +10,15 @@ import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod.EventBusSubscriber(modid = "futurefireproof")
+@Mod.EventBusSubscriber(modid = FutureFireproof.MOD_ID)
 public class CommonEventHandler {
     @SubscribeEvent
     public static void onEntityJoinWorld(EntityJoinWorldEvent event) {
-        World world = event.getWorld();
-        if (RegistrarFF.isRealDropsLoaded && (event.getEntity().getClass() == EntityItem.class || event.getEntity().getClass().toString().equals("realdrops.entities.EntityItemLoot"))) {
-            EntityItem entityItem = (EntityItem) event.getEntity();
-            ItemStack stack = entityItem.getItem();
-            if (!stack.isEmpty() && FireproofHelper.isFireproofItem(stack)) {
-                EntityItem fireproofItem = EntityFireproofItemLootHandler.getEntityItem(entityItem);
-                if (fireproofItem != null) {
-                    event.getWorld().spawnEntity(fireproofItem);
-                    entityItem.setDead();
-                    event.setCanceled(true);
-                    return;
-                }
-            }
-        }
         if (event.getEntity().getClass() == EntityItem.class) {
             EntityItem entityItem = (EntityItem) event.getEntity();
             ItemStack stack = entityItem.getItem();
             if (!stack.isEmpty() && FireproofHelper.isFireproofItem(stack)) {
-                EntityFireproofItem fireproofItem = new EntityFireproofItem(world, entityItem, stack);
+                EntityFireproofItem fireproofItem = new EntityFireproofItem(event.getWorld(), entityItem, stack);
                 event.getWorld().spawnEntity(fireproofItem);
                 entityItem.setDead();
                 event.setCanceled(true);
